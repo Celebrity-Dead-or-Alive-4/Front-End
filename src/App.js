ece87d7af188cc04b1aa0879ca340f74c8f32cce
Carlos Turcios <turcios50@gmail.com>
2019-12-18 20:35:04 -0800

    imported react-hook-form and started the form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-
+import { Route } from 'react-router-dom';
 import './App.css';
+
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
       <h1>App is running</h1>
+      <Route path="/login" component={Login} />
     </div>
   );
 }
