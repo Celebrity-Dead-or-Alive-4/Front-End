4e63bf6e6ab8f931da21eab2207d94c6d675ab22
Carlos Turcios <turcios50@gmail.com>
2019-12-19 20:52:26 -0800

    validation complete

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,30 +4,38 @@ import { Route } from 'react-router-dom';
 import useForm from 'react-hook-form';
 
 function Login() {
-    const { register, handleSubmit } = useForm()
+    const { register, handleSubmit, errors, setValue, reset } = useForm()
+    const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
     const onSubmit = data => console.log(data)
+
+    const validateUserName = async value => {
+        await sleep(1000);
+        if (value === "username") return true;
+        return false;
+    }
+    const validatePassword = async value => {
+        await sleep(1000);
+        if (value === "password") return true;
+        return false;
+    }
+
     return (
+        <div className="Login-form">
+            <form onSubmit={handleSubmit(onSubmit)}>
 
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <label>Username:</label>
-            <input name="Username" ref={register} />
-            <label>Password:</label>
-            <input name="password" ref={register} />
-
-
-
-            <button type="submit">Log in</button>
-
-
-
-
-
-
-
-
-        </form>
-
-
+                <label className="username">Username:</label>
+                <input name="username" ref={register({ required: true, maxLength: 20, validate: validateUserName })} />
+                {errors.username && errors.username.type === "required" && (
+                    <p>This is required</p>
+                )}
+                <label className="password">Password:</label>
+                <input name="password" ref={register({ required: true, maxLength: 20, validate: validatePassword })} />
+                {errors.password && errors.password.type === "required" && (
+                    <p>This is required</p>
+                )}
+                <button type="submit">Log in</button>
+            </form>
+        </div>
 
 
     )
