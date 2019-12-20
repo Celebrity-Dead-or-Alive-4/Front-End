b112ebd4b26fc58e5338dedfaa529704242c72fd
cladams0203 <cladams0203@gmail.com>
2019-12-20 12:13:12 -0500

    added state and dispatch to app with connect

--- src/utils/actions.js
+++ src/utils/actions.js
@@ -9,6 +9,28 @@ export const REGISTER_START = 'REGISTER_START'
 export const REGISTER_SUCCESS = 'REGISTER_SUCCESS'
 export const REGISTER_FAILURE = 'REGISTER_FAILURE'
 
-export function handleLogin(input) {
+export const loginAction = (input) => dispatch => {
+    dispatch({type: LOGIN_START })
+    axiosWithAuth()
+        .post('/api/login', input)
+        .then(res => {
+            localStorage.setItem('token', res.dataheader)
+            dispatch({ type: LOGIN_SUCCESS, payload: res.data })
+        })
+        .catch(err => {
+            dispatch({ type: LOGIN_FAILURE, payload: err })
+        })
+}
 
+export const registerAction = (input) => dispatch => {
+    dispatch({ type: REGISTER_START })
+    axiosWithAuth()
+        .post('/api/register', input)
+        .then(res => {
+            localStorage.setItem('token', res.dataheader)
+            dispatch({ type: REGISTER_SUCCESS, payload: res.data })
+        })
+        .catch(err => {
+            dispatch({ type: REGISTER_FAILURE, payload: err })
+        })
 }
