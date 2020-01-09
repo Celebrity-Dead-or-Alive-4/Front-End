af70067183f8799e82dbda4f092648de324626ec
LittleBoGeek <NadiaChilds@outlook.com>
2020-01-08 22:50:02 -0500

    updated register

--- src/components/Register.js
+++ src/components/Register.js
@@ -36,7 +36,7 @@ font-weight:100;
   Username: yup.string().required('username is required'),
   Password: yup.string().required('password is required'),
   Email:yup.string().email('invalid email').required('email is required'),
-  Name: yup.string.required('name is required')
+  Name: yup.string().required('name is required')
 })
 
 
@@ -70,26 +70,18 @@ return (
 
 
 <form className="App" onSubmit={handleSubmit(onSubmit)}>
-
-
 <StyledLabel> User Name: </StyledLabel>
 <StyledInput type="text" name ="firstName" ref={register({required:true})} />
 <StyledLabel > Email </StyledLabel>
-
 <StyledInput name="email" ref={register({required:true})} /> 
-
 <StyledLabel> Password</StyledLabel>
 <StyledInput type="text"  name="password" ref={register({required:true})} /> {<br></br>}
 <StyledLabel> Name </StyledLabel>
-<StyledInput type="text" name="name" ref={register({required:true})} /> 
-
+<StyledInput type="text" name="name" ref={register({required:true})} /> {<br></br>}
 <div><StyledH3> Yes, I'd like to receive emails about app updates and special offers </StyledH3>
-
  <input type="checkbox" /> </div>
-
  <Button> Register</Button>
- 
-</form>
+ </form>
 
   
 )
